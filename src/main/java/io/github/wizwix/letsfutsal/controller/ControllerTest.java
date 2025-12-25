f21d6b251d999ceb4d7e06c8be35915316321b8a
WizWix <56517837+WizWix@users.noreply.github.com>
2025-12-25 18:25:45 +0900

    chore: '/test' 경로에 대한 간략한 설명 추가

--- src/main/java/io/github/wizwix/letsfutsal/controller/ControllerTest.java
+++ src/main/java/io/github/wizwix/letsfutsal/controller/ControllerTest.java
@@ -2,18 +2,16 @@ package io.github.wizwix.letsfutsal.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequiredArgsConstructor
 public class ControllerTest {
-
+  /// Test code. If you can't access `localhost:8080/test` or don't see `aaaaaaaaaaaaaaaa` in your browser, something is wrong about Tomcat or Spring MVC configuration.
   @GetMapping("/test")
   @ResponseBody
-  public String list(Model model) {
+  public String list() {
     return "aaaaaaaaaaaaaaaa";
   }
-
 }
