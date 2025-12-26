8ae0a158baff6e807d8be5c940d4e1b65abd188a
WizWix <56517837+WizWix@users.noreply.github.com>
2025-12-26 21:18:17 +0900

    wip: MySQL 구문 수정
    - entity 테이블 수정
    - entity 테이블 관련 Trigger 추가

--- sql/create_user.sql
+++ sql/create_user.sql
@@ -1,6 +1,14 @@
+-- Drop user and schema
+drop database if exists letsfutsal;
+drop user if exists letsfutsal@localhost;
+flush privileges;
+
 -- Create new user 'letsfutsal' and database 'letsfutsal'
 -- Do this only once per development DBMS
-create database `letsfutsal`;
-create user `letsfutsal`@`localhost` identified by 'letsfutsal';
-grant all privileges on `letsfutsal`.* to `letsfutsal`@`localhost`;
+create database letsfutsal;
+create user if not exists letsfutsal@localhost identified by 'letsfutsal';
+grant all privileges on letsfutsal.* to letsfutsal@localhost;
+grant system_variables_admin, super on *.* to letsfutsal@localhost;
 flush privileges;
+-- To allow letsfutsal user to create trigger, this must be set to 1 :(
+set persist log_bin_trust_function_creators = 1;
